4d8a98bb2af9e31d85578afac29aed54e2ec6ae3
Tim Shelburne <shelburt02@gmail.com>
2019-11-28 01:46:10 -0500

    updated index with recent additions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 export {default, Client, RequestError} from './client'
-export {default as json} from './middlewares/json'
 export {default as bearerAuth} from './middlewares/bearer-auth'
+export {default as blob} from './middlewares/blob'
+export {default as form} from './middlewares/form'
+export {default as html} from './middlewares/html'
+export {default as json} from './middlewares/json'
