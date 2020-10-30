15ce681e650cb2019435317df6a10118cbfa3566
Tim Shelburne <shelburt02@gmail.com>
2020-10-30 16:20:00 -0400

    Added comment explaining silliness

--- test/setup-jsdom.js
+++ test/setup-jsdom.js
@@ -1,6 +1,7 @@
 import jsdom from 'jsdom-global'
 
 before(function() {
+	// this looks dumb. it is dumb. but it is what allows isomorphic-form-data to provide FormData for our tests
 	const originalFormData = FormData
 	this.jsdom =  new jsdom(``, {
 		url: "https://test.com/",
