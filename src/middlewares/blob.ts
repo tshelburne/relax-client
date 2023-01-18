2209d42b9d987e6a2b443f900685083deb7976ba
Tim Shelburne <shelburt02@gmail.com>
2023-01-18 00:53:27 -0500

    Updated middlewares for new types

--- src/middlewares/blob.ts
+++ src/middlewares/blob.ts
@@ -1,6 +1,6 @@
 import {Middleware} from '../client'
 
-function blob(): Middleware<Blob, Response> {
+function blob(): Middleware<Response, Blob> {
 	return async (_, next) => {
 		const response = await next({ headers: { Accept: `*/*` } })
 		return response.status === 204 ? new Blob([]) : response.blob()
